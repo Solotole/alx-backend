cacb5dbd19592f21e050dc64de873828521938b2
Solotole <solomontoleak47@gmail.com>
2024-09-10 13:32:29 +0100

    Get locale from reques

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Flask app development"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+#from typing import List
+
+
+app = Flask(__name__)
+
+class Config:
+    """a basic configuring class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """index python file"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
