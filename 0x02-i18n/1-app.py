790eb442e66079027d9d9b6aecf6de984997f03d
Solotole <solomontoleak47@gmail.com>
2024-09-10 13:31:13 +0100

    Babel configuration

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Flask app development"""
+from flask import Flask, render_template
+from flask_babel import Babel
+#from typing import List
+
+
+app = Flask(__name__)
+
+class Config:
+    """a basic configuring class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """index python file"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
